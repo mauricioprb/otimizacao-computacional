73578467bba6b5db2618dcefddf53e03e4b19fea
Mauricio Pereira <mauriciobraga100@gmail.com>
2023-03-07 12:02:53 -0300

    Refatorado

--- resolver-sistema/refatorado.py
+++ resolver-sistema/refatorado.py
@@ -1,52 +1,86 @@
-import re # Importa a biblioteca para expressões regulares (regular expression)
+import re  # Módulo para expressões regulares
 
-nomesVariaveis = {}
+nomes_variaveis = {}  # Dicionário com o nome das variáveis
 
-# Função para inserir uma variável no dicionário de nomes de variáveis
-def insereVariavel(variavel):
-  if variavel not in nomesVariaveis:
-    nomesVariaveis[variavel]='sim'
 
-# Função para separar o coeficiente e a variável de um termo da equação
-def separaCoef_var(termo):
-  coef = ''
-  variavel = ''
-  indice=0
-  for letra in termo:
-    if(letra.isdigit()):
-      coef=coef+letra
-      indice+=1 # Incrementa o índice
-    else:
-      if indice==0:# A primeira letra não é número
-        coef='1'
-      variavel = termo[indice:]
-      break # Sai do for
-  return coef,variavel
+def retorna_coeficiente(linha, pos):
+    valor = []
+    pc = pos-1
+    while pc >= 0:
+        if linha[pc] == '+' or linha[pc] == '-':
+            if linha[pc] == '-':
+                valor.append(linha[pc])  # Adiciona '-' ao valor
+                if len(valor) == 1:
+                    valor.insert(0, '1')  # Adiciona '1' antes do '-'
+                break
+            else:
+                if len(valor) == 0:
+                    valor.append('1')
+                break
+        valor.append(linha[pc])
+        pc -= 1  # move-se para a esquerda
+    valor.reverse()
+    return ''.join(valor)
 
-# Função para abrir e ler o arquivo
-def abre_arquivo(nomeArquivo):
-  try:
-    with open(nomeArquivo) as dados:
-      for linha in dados:# Percorre linha a linha o arquivo
+
+def insere_variavel(variavel):
+    if variavel not in nomes_variaveis:
+        nomes_variaveis[variavel] = 'sim'
+
+
+def separa_coef_var(termo):
+    coef = ''
+    variavel = ''
+    indice = 0
+    for letra in termo:
+        if letra.isdigit():
+            coef += letra
+            indice += 1
+        else:
+            if indice == 0:  # A primeira letra não é um número
+                coef = '1'
+            variavel = termo[indice:]
+            break
+    return coef, variavel
+
+
+def monta_coeficientes(linha_entrada):
+    variaveis = nomes_variaveis.keys()
+    for nome_variavel in variaveis:
+        pos = linha_entrada.find(nome_variavel)
+        coef = -100
+        if pos < 0:  # Nome de variável não encontrado na linha
+            coef = 0
+        else:
+            if pos == 0:  # Nome de variável encontrado no início da linha
+                coef = 1
+            else:
+                if pos == 1:  # Nome de variável está na posição 1
+                    if linha_entrada[pos-1] == '-':
+                        coef = -1
+                    else:
+                        coef = int(linha_entrada[pos-1])
+                else:  # o nome da variável está na posição > 1
+                    # tratamento do coeficiente
+                    coef = retorna_coeficiente(linha_entrada, pos)
+            print(nome_variavel, ':', pos, ' c:', coef)
+
+
+def abre_arquivo(nome_arquivo):
+    dados = open(nome_arquivo)
+    for linha in dados:  # Percorre linha a linha o arquivo
         print(linha)
-        elementos = linha.split('=') # Quebra a linha no igual
-        le = elementos[0]
-        ld = elementos[1]
-        termos = re.split('-|\+',le) # Separa a equação em termos
+        elementos = linha.split('=')  # Quebra a linha no igual
+        lado_esquerdo = elementos[0]
+        lado_direito = elementos[1]
+        termos = re.split('-|\+', lado_esquerdo)
         for termo in termos:
-          if len(termo)>0:
-            c,v = separaCoef_var(termo.lstrip().rstrip())
-            insereVariavel(v) # Adiciona a variável no dicionário
-          print('t:',termo)
-        
-        print(nomesVariaveis.keys()) # Imprime as chaves do dicionário de nomes de variáveis
+            if len(termo) > 0:
+                c, v = separa_coef_var(termo.strip())
+                insere_variavel(v)
+        monta_coeficientes(linha)
 
-  except FileNotFoundError:
-    print("Arquivo não encontrado!")
 
-# Chamada da função para abrir e ler o arquivo
-abre_arquivo('resolver-sistema\equacoes.txt')
-
-# Chamada da função separaCoef_var para testar um termo
-c,v = separaCoef_var('x1')
-print('Coeficiente: ',c,'Variavel:',v)
+abre_arquivo('resolver-sistema/equacoes.txt')
+c, v = separa_coef_var('x1')
+print('Coeficiente: ', c, 'Variável:', v)
